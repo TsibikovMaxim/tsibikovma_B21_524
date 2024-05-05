7bf16e36e229f6b79c5a4a6a0ed872041b698d75
Maxim Tsibikov <maksim.cibikov@iss.digital>
2024-05-05 12:48:24 +0300

    lab 6

--- 6sem/results/main.py
+++ 6sem/results/main.py
@@ -3,11 +3,11 @@ import utils
 
 def main():
     # Расчёта горизонтального и вертикального профиля изображения
-    utils.create_profiles('x')
-    utils.create_profiles('y')
+    # utils.create_profiles('x')
+    #utils.create_profiles('y')
 
     # Сегментация символов
-    utils.symbol_selection()
+    #utils.symbol_selection()
 
     # Сегментация символов
     utils.text_selection()
